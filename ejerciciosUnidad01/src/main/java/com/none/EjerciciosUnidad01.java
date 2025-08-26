e1b01208c2555e6972a36ab47fbcc9f0f25086d2
https-michi <glitchmichi@gmail.com>
2025-08-26 13:50:04 -0500

    chore(init): crear repositorio para ejercicios de la unidad 01

---
+++ ejerciciosUnidad01/src/main/java/com/none/EjerciciosUnidad01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.none;
+
+/**
+ *
+ * @author snowden
+ */
+public class EjerciciosUnidad01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
