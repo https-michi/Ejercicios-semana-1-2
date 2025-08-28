1bb6e452565ec04090f3ff00d6bbbb63edc91812
edwardnunezv <esnunezva@gmail.com>
2025-08-28 12:48:12 -0500

    Agrego ejercicio4

---
+++ ejerciciosUnidad01/src/main/java/com/none/semana02/ejercicio4/Ejercicio4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio4;
+
+/**
+ *
+ * @author Edward Núñez
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        TrianguloRectangulo triangulo1 = new TrianguloRectangulo(3, 4);
+        triangulo1.mostrarDatos();
+    }
+}
